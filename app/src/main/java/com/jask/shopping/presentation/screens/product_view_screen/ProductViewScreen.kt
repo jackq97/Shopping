fc75f4a1d058a4a4d124b4e132bc1caba3594935
jassi <jaskq@proton.me>
2024-03-12 17:57:18 +0530

    done_product_view_screen

--- app/src/main/java/com/jask/shopping/presentation/screens/product_view_screen/ProductViewScreen.kt
+++ app/src/main/java/com/jask/shopping/presentation/screens/product_view_screen/ProductViewScreen.kt
@@ -1,13 +1,20 @@
 package com.jask.shopping.presentation.screens.product_view_screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -22,6 +29,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -33,6 +41,9 @@ import com.google.accompanist.pager.rememberPagerState
 fun ProductViewScreen() {
 
     val pagerState = rememberPagerState()
+
+    val listOfColors = listOf(Color.Black, Color.Red, Color.Gray, Color.Blue, Color.Magenta, Color.Yellow)
+    val listOfSizes = listOf("XL", "S", "M", "L")
 
     data class Page(val url: String)
 
@@ -92,32 +103,115 @@ fun ProductViewScreen() {
                 )
             }
 
-            Row(verticalAlignment = Alignment.Bottom) {
-                Text(text = "Scotch Premium",
-                    style = MaterialTheme.typography.h1
-                )
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+
                 Text(
-                    modifier = Modifier.align(Alignment.End),
-                    text = "1200",
-                    style = MaterialTheme.typography.body1
-                    )
+                    text = "Scotch Premium",
+                    style = MaterialTheme.typography.h4)
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    text = "$1200",
+                    style = MaterialTheme.typography.body1,
+                    fontSize = 25.sp
+                )
 
             }
 
             Divider(color = Color.Gray)
 
             Row {
+                ColorsInfoColumn(
+                    modifier = Modifier.weight(1f),
+                    listOfColors = listOfColors)
 
+                Spacer(modifier = Modifier.width(10.dp))
+
+                SizeColumnInfo(
+                    modifier = Modifier.weight(1f),
+                    listOfSizes = listOfSizes)
             }
 
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Add To Card")
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(4.dp),
+                onClick = { /*TODO*/ }) {
+                Text(text = "Add To Card",
+                style = MaterialTheme.typography.button
+                )
             }
 
         }
     }
 }
 
+@Composable
+fun ColorsInfoColumn(
+    modifier: Modifier,
+    listOfColors: List<Color>) {
+    Column(modifier = modifier) {
+        Text(
+            modifier = Modifier.padding(start = 5.dp),
+            text = "Color",
+            style = MaterialTheme.typography.h6
+        )
+        LazyRow {
+            items(listOfColors){ colors ->
+
+                Box(modifier = Modifier.padding(horizontal = 5.dp)) {
+                    Box(
+                        modifier = Modifier
+                            .size(32.dp)
+                            .padding(horizontal = 4.dp)
+                            .background(
+                                color = colors,
+                                shape = CircleShape
+                            )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SizeColumnInfo(
+    modifier: Modifier,
+    listOfSizes: List<String>) {
+    Column(modifier = modifier) {
+        Text(
+            modifier = Modifier.padding(start = 5.dp),
+            text = "Size",
+            style = MaterialTheme.typography.h6
+        )
+        LazyRow {
+
+            items(listOfSizes){ sizes ->
+
+                Box(modifier = Modifier.padding(horizontal = 5.dp)) {
+                    Box(
+                        modifier = Modifier
+                            .size(32.dp)
+                            .padding(horizontal = 4.dp)
+                            .background(
+                                color = Color.Gray,
+                                shape = CircleShape
+                            )
+                    ) {
+                        Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            text = sizes)
+                    }
+                }
+            }
+        }
+    }
+}
 
 @Composable
 @Preview
